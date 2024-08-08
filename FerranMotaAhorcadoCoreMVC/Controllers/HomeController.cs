1216f5016913d1dcc52c764ebecba4ddc118bb91
ferrym96 <ferrym96fml@gmail.com>
2024-08-08 09:31:14 +0200

    Agregar archivos de proyecto.

---
+++ FerranMotaAhorcadoCoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using FerranMotaAhorcadoCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FerranMotaAhorcadoCoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Renamed action for the Hangman game
+        public IActionResult Ahorcado()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
